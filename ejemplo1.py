b9ab31134c140959f33980d32c8a6dc4f54cb315
Santiago Bonacci <sbonacci89@gmail.com>
2025-05-19 18:09:06 -0300

    Versión 2.0 se cambiaron aspectos en el ejemplo

--- ejemplo1.py
+++ ejemplo1.py
@@ -7,5 +7,7 @@ amigos = ["Juan", "Pedro", "Maria", "Ana", "Luis", "Carlos", "Laura", "Sofia", "
 
 # Mostramos el nombre elegido con un mensaje más claro
 nombre_aleatorio = obtener_nombres_aleatorios(amigos)
+print("El nombre elegido al azar es:")
 print(f"Nombre elegido al azar: {nombre_aleatorio}")
-
+print("El premio debe ser retirado en la tienda de la esquina.")
+# Fin del programa  
