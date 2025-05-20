7fb505854498ee7bca0cff0f0fcb09e2ebb9546d
Santiago Bonacci <sbonacci89@gmail.com>
2025-05-20 17:20:47 -0300

    Versión 3.0 con inicio y poema

--- proyecto/inicio/urls.py
+++ proyecto/inicio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio_view, name='inicio'),
+    path('', views.inicio_view, name='inicio'),        # muestra inicio.html
+    path('saludo/', views.saludo_view, name='saludo'),  # muestra poema.html
 ]
--- proyecto/inicio/views.py
+++ proyecto/inicio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def inicio_view(request):
+    return render(request, 'inicio.html')
+
+def saludo_view(request):
+    return render(request, 'poema.html')
+
