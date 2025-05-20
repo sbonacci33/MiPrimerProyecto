96af2ab3968fc7700b10826055e4341482dedbcd
Santiago Bonacci <sbonacci89@gmail.com>
2025-05-20 16:31:15 -0300

    Agrego app 'inicio' con vista y template principal

--- proyecto/proyectobonacci/urls.py
+++ proyecto/proyectobonacci/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import index
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("saludo/", index),
+    path('', include('inicio.urls')),  # conecta la raíz con la app 'inicio'
+    path('admin/', admin.site.urls), 
 ]
+
 
 
--- proyecto/proyectobonacci/views.py
+++ proyecto/proyectobonacci/views.py
@@ -1,5 +1,7 @@
+#NO ESTA EN USO PORQUE SE CREÓ UNA APP PARA INICIO
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'template.html')
+    return render(request, 'inicio.html')
